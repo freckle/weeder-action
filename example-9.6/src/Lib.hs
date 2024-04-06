11f7e0eb4cc4b09c274a61f6b5b21ef68621752e
patrick brisbin <pbrisbin@gmail.com>
2024-04-06 13:07:12 -0400

    Add 9.6 example, use matrix

---
+++ example-9.6/src/Lib.hs
@@ -0,0 +1,16 @@
+module Lib
+    ( helloWorld
+    , goodbyeWorld
+    , thisFunction
+    ) where
+
+import Prelude
+
+helloWorld :: String
+helloWorld = "Hello world"
+
+goodbyeWorld :: String
+goodbyeWorld = "Goodbye world"
+
+thisFunction :: String
+thisFunction = "Is unused"
